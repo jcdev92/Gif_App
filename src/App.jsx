3f97d7a84c31e22217d633320cb521564cf8e3ef
Jesus Clemente <jcdeveloper92@gmail.com>
2022-10-08 18:44:20 -0400

    form input manger added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import GifExpertApp from "./components/GifExpertApp.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+        <GifExpertApp />
     </div>
   )
 }
