7d61975c5ada9f80ba3f8dab1ed10fa024ee3ad6
Jesus Clemente <jcdeveloper92@gmail.com>
2022-10-12 14:17:44 -0400

    rendering gifs  content

--- src/components/GridGif.jsx
+++ src/components/GridGif.jsx
@@ -1,15 +1,28 @@
-import React     from "react";
-import {getGifs} from "../helpers/getGif.js";
-
+import React , { useEffect , useState } from "react";
+import { getGifs }                      from "../helpers/getGif.js";
 
 export const GridGif = ({ category, key }) => {
+  const [images, setImages] = useState([]);
 
-  getGifs(category);
+  const getImages = async () => {
+    const images = await getGifs(category);
+    setImages(images);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
 
   return (
     <>
-      <div key={key}>
-        <h2>{category}</h2>
+      <h2>{category}</h2>
+      <div className="card-grid">
+        {images.map(({id, title, url}) => (
+          <div key={id} className="card">
+            <img src={url} alt={title} />
+            <p>{title}</p>
+          </div>
+        ))}
       </div>
     </>
   );
