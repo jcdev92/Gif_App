cf39642d01f5d120e4e6072d248bddea9aaf5e7e
Jesus Clemente <jcdeveloper92@gmail.com>
2022-10-13 19:01:55 -0400

    testing gif item

--- babel.config.cjs
+++ babel.config.cjs
@@ -3,4 +3,7 @@ module.exports = {
         [ '@babel/preset-env', { targets: { esmodules: true } } ],
         [ '@babel/preset-react', { runtime: 'automatic' } ],
     ],
+    "plugins": [
+    ["babel-plugin-transform-import-meta", { "module": "ES6" }]
+    ]
 };
